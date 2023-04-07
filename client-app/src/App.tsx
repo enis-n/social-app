2682bfb0601eb2214fa3c7a756586bc9e2d361c4
enis nishori <en46627@ubt-uni.net>
2023-04-07 16:12:58 +0200

    Fetching data with axios, and added semantic ui

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/posts').then(response => {
+      console.log(response);
+      setPosts(response.data);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h2' icon='users' content='UBT Social App' />
+      <List>
+        {posts.map((post: any) => (
+          <List.Item key={post.id}>
+            {post.title}
+          </List.Item>
+        ))}
+      </List>
     </div>
   );
 }
