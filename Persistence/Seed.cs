cc0a5187581753bf95116a10e51e5792e542296f
en47902 <emanuelnov19@gmail.com>
2023-06-03 22:51:21 +0200

    Updated the Seed data

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -11,7 +11,7 @@ namespace Persistence
     {
         public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
-            if(!userManager.Users.Any())
+            if (!userManager.Users.Any() && !context.Posts.Any())
             {
                 var users = new List<AppUser>
                 {
@@ -24,105 +24,202 @@ namespace Persistence
                 {
                     await userManager.CreateAsync(user, "Passw0rd");
                 }
+
+                var posts = new List<Post>
+                {
+                    new Post
+                    {
+                        Title = "Posting For Lab 1 Post Number 1",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Post 2 months ago",
+                        Category = "drinks",
+                        Venue = "Pub",
+                        Attendees = new List<PostAttendee>
+                            {
+                                new PostAttendee
+                                {
+                                    AppUser = users[0],
+                                    isHost = true
+                                },
+                                new PostAttendee
+                                {
+                                    AppUser = users[1],
+                                    isHost = false
+                                }
+                            }
+                    },
+                    new Post
+                    {
+                        Title = "Posting For Lab 1 Post Number 2",
+                        Date = DateTime.Now.AddMonths(-1),
+                        Description = "Post 1 month ago",
+                        Category = "culture",
+                        Venue = "Louvre",
+                        Attendees = new List<PostAttendee>
+                            {
+                                new PostAttendee
+                                {
+                                    AppUser = users[0],
+                                    isHost = true
+                                },
+                                new PostAttendee
+                                {
+                                    AppUser = users[2],
+                                    isHost = false
+                                }
+                            }
+                    },
+                    new Post
+                    {
+                        Title = "Posting For Lab 1 Post Number 3",
+                        Date = DateTime.Now.AddMonths(1),
+                        Description = "Post 1 month in future",
+                        Category = "culture",
+                        Venue = "Natural History Museum",
+                        Attendees = new List<PostAttendee>
+                            {
+                                new PostAttendee
+                                {
+                                    AppUser = users[1],
+                                    isHost = true
+                                },
+                                new PostAttendee
+                                {
+                                    AppUser = users[0],
+                                    isHost = false
+                                }
+                            }
+                    },
+                    new Post
+                    {
+                        Title = "Posting For Lab 1 Post Number 4",
+                        Date = DateTime.Now.AddMonths(2),
+                        Description = "Post 2 months in future",
+                        Category = "music",
+                        Venue = "O2 Arena",
+                        Attendees = new List<PostAttendee>
+                            {
+                                new PostAttendee
+                                {
+                                    AppUser = users[1],
+                                    isHost = true
+                                },
+                                new PostAttendee
+                                {
+                                    AppUser = users[0],
+                                    isHost = false
+                                }
+                            }
+                    },
+                    new Post
+                    {
+                        Title = "Posting For Lab 1 Post Number 5",
+                        Date = DateTime.Now.AddMonths(3),
+                        Description = "Post 3 months in future",
+                        Category = "drinks",
+                        Venue = "Another pub",
+                        Attendees = new List<PostAttendee>
+                            {
+                                new PostAttendee
+                                {
+                                    AppUser = users[1],
+                                    isHost = true
+                                },
+                                new PostAttendee
+                                {
+                                    AppUser = users[0],
+                                    isHost = false
+                                }
+                            }
+                    },
+                    new Post
+                    {
+                        Title = "Posting For Lab 1 Post Number 6",
+                        Date = DateTime.Now.AddMonths(4),
+                        Description = "Post 4 months in future",
+                        Category = "drinks",
+                        Venue = "Yet another pub",
+                        Attendees = new List<PostAttendee>
+                            {
+                                new PostAttendee
+                                {
+                                    AppUser = users[1],
+                                    isHost = true
+                                },
+                                new PostAttendee
+                                {
+                                    AppUser = users[0],
+                                    isHost = false
+                                }
+                            }
+                    },
+                    new Post
+                    {
+                        Title = "Posting For Lab 1 Post Number 6",
+                        Date = DateTime.Now.AddMonths(5),
+                        Description = "Post 5 months in future",
+                        Category = "drinks",
+                        Venue = "Just another pub",
+                        Attendees = new List<PostAttendee>
+                            {
+                                new PostAttendee
+                                {
+                                    AppUser = users[0],
+                                    isHost = true
+                                },
+                                new PostAttendee
+                                {
+                                    AppUser = users[1],
+                                    isHost = false
+                                }
+                            }
+                    },
+                    new Post
+                    {
+                        Title = "Posting For Lab 1 Post Number 7",
+                        Date = DateTime.Now.AddMonths(6),
+                        Description = "Post 6 months in future",
+                        Category = "music",
+                        Venue = "Roundhouse Camden",
+                        Attendees = new List<PostAttendee>
+                            {
+                                new PostAttendee
+                                {
+                                    AppUser = users[1],
+                                    isHost = true
+                                },
+                            }
+                    },
+                    new Post
+                    {
+                        Title = "Posting For Lab 1 Post Number 8",
+                        Date = DateTime.Now.AddMonths(7),
+                        Description = "Post 2 months ago",
+                        Category = "travel",
+                        Venue = "Somewhere on the Thames",
+                        Attendees = new List<PostAttendee>
+                            {
+                                new PostAttendee
+                                {
+                                    AppUser = users[2],
+                                    isHost = true
+                                },
+                            }
+                    },
+                };
+
+                await context.Posts.AddRangeAsync(posts);
+                await context.SaveChangesAsync();
             }
-            await SeedPosts(context);
+
             await SeedStories(context);
         }
 
-        public static async Task SeedPosts(DataContext context){
-            if (context.Posts.Any()) return;
+       
+        public static async Task SeedStories(DataContext context)
+        {
 
-            var posts = new List<Post>
-            {
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 1",
-                    Date = DateTime.Now.AddMonths(-2),
-                    Description = "Post 2 months ago",
-                    Category = "drinks",
-                    Venue = "Pub",
-                },
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 2",
-                    Date = DateTime.Now.AddMonths(-1),
-                    Description = "Post 1 month ago",
-                    Category = "culture",
-                    Venue = "Louvre",
-                },
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 3",
-                    Date = DateTime.Now.AddMonths(1),
-                    Description = "Post 1 month in future",
-                    Category = "culture",
-                    Venue = "Natural History Museum",
-                },
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 4",
-                    Date = DateTime.Now.AddMonths(2),
-                    Description = "Post 2 months in future",
-                    Category = "music",
-                    Venue = "O2 Arena",
-                },
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 5",
-                    Date = DateTime.Now.AddMonths(3),
-                    Description = "Post 3 months in future",
-                    Category = "drinks",
-                    Venue = "Another pub",
-                },
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 6",
-                    Date = DateTime.Now.AddMonths(4),
-                    Description = "Post 4 months in future",
-                    Category = "drinks",
-                    Venue = "Yet another pub",
-                },
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 6",
-                    Date = DateTime.Now.AddMonths(5),
-                    Description = "Post 5 months in future",
-                    Category = "drinks",
-                    Venue = "Just another pub",
-                },
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 7",
-                    Date = DateTime.Now.AddMonths(6),
-                    Description = "Post 6 months in future",
-                    Category = "music",
-                    Venue = "Roundhouse Camden",
-                },
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 8",
-                    Date = DateTime.Now.AddMonths(7),
-                    Description = "Post 2 months ago",
-                    Category = "travel",
-                    Venue = "Somewhere on the Thames",
-                },
-                new Post
-                {
-                    Title = "Posting For Lab 1 Post Number 9",
-                    Date = DateTime.Now.AddMonths(8),
-                    Description = "Post 8 months in future",
-                    Category = "film",
-                    Venue = "Cinema",
-                }
-            };
-
-            await context.Posts.AddRangeAsync(posts);
-            await context.SaveChangesAsync();
-        }
-
-        public static async Task SeedStories(DataContext context){
-         
-            if(context.Stories.Any()) return;
+            if (context.Stories.Any()) return;
 
             var stories = new List<Story>
             {
