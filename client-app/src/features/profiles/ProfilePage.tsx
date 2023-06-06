8ae2df0bc77a4969038090e57efc5b39ca279d57
enis nishori <en46627@ubt-uni.net>
2023-06-06 18:23:15 +0200

    Added Profile Content

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import ProfileHeader from './ProfileHeader'
+import ProfileContent from './ProfileContent'
+
 
 export default function ProfilePage() {
     return (
         <Grid>
             <Grid.Column width={16}>
                 <ProfileHeader />
+                <ProfileContent />
             </Grid.Column>
         </Grid>
     )
