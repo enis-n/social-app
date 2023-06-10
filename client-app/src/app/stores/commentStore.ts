0d264d942791821da387cdd8900cb280372dfa6d
enis nishori <en46627@ubt-uni.net>
2023-06-10 13:37:43 +0200

    Added signalR in the front, commentStore

---
+++ client-app/src/app/stores/commentStore.ts
@@ -0,0 +1,44 @@
+import { HubConnection, HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import { ChatComment } from "../models/comment";
+import { makeAutoObservable, runInAction } from "mobx";
+import { store } from "./store";
+
+export default class CommentStore {
+    comments: ChatComment[] = [];
+    hubConnection: HubConnection | null = null;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    createHubConnection = (postId: string) => {
+        if (store.postStore.selectedPost) {
+            this.hubConnection = new HubConnectionBuilder()
+                .withUrl('http://localhost:5000/chat?postId=' + postId, {
+                    accessTokenFactory: () => store.userStore.user?.token!
+                })
+                .withAutomaticReconnect()
+                .configureLogging(LogLevel.Information)
+                .build();
+
+            this.hubConnection.start().catch(error => console.log('Error establishing the connection: ', error));
+
+            this.hubConnection.on('LoadComments', (comments: ChatComment[]) => {
+                runInAction(() => this.comments = comments);
+            })
+
+            this.hubConnection.on('ReceiveComments', (comment: ChatComment) => {
+                runInAction(() => this.comments.push(comment));
+            })
+        }
+    }
+
+    stopHubConnection = () => {
+        this.hubConnection?.stop().catch(error => console.log('Error stopping connection: ', error));
+    }
+
+    clearComments = () => {
+        this.comments = [];
+        this.stopHubConnection();
+    }
+}
